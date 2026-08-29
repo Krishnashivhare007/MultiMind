1b16020d3e3e50be31f4c4d4b5b18f5a1ae2ef4b
Krishnashivhare007 <krishnashivhare197@gmail.com>
2026-08-29 11:42:04 +0530

    feat: implement PPT generation functionality; add S3 upload and download utilities, enhance presentation design with custom slides

--- backend/services/agent/agents/ppt.agent.js
+++ backend/services/agent/agents/ppt.agent.js
@@ -1,3 +1,86 @@
-export const pptAgent = async () => {
-    
+import { getModel } from "../config/llmModels.js"
+import { generatePPT } from "../utils/generatePpt.js"
+import { getFromS3 } from "../utils/getFromS3.js"
+import { uploadToS3 } from "../utils/uploadTOS3.js"
+
+export const pptAgent = async (state) => {
+    try {
+        const llm = await getModel("ppt")
+        const prompt = `
+        You are a professional presentation designer.
+
+        Return ONLY valid JSON.
+
+        format:
+
+        {
+        "title":"",
+        "subtitle":"",
+        "slides":[
+        {
+        "title":"",
+        "points":[
+        "",
+        "",
+        "",
+        ""
+        ]
+        }
+        ]
+        }
+
+        Rules:
+
+        - Generate exactly 6 content slides.
+        - Each slide should have 4-6 concise bullet points.
+        - No markdown.
+        - No explanation.
+        - No code block.
+        - Return ONLY JSON.
+
+        Topic:
+
+        ${state.prompt}
+        `
+
+        const res = await llm.invoke(prompt)
+        
+        const data = JSON.parse(res.content)
+
+        const ppt =await generatePPT(data)
+
+        const buffer = await ppt.write({
+            outputType:"nodebuffer"
+        })
+        console.log("ye rha buffer: ",buffer);
+        
+
+        const fileName = `ppt-${Date.now()}.pptx`
+
+        await uploadToS3(fileName,buffer,"application/vnd.openxmlformats-officedocument.presentationml.presentation")
+
+        const downloadUrl =await getFromS3(fileName,24*60)
+
+        return {
+            ...state,
+            aiResponse:`#PPT Generated
+            
+**${data.title}**
+
+⬇️ [Download PPT](${downloadUrl})
+
+⏳ Link expires in 24 hours
+            `
+        }
+
+        
+    } catch (error) {
+        console.log(error);
+        return {
+             ...state,
+             aiResponse:`
+             ❌ Failed to generate PPT
+             `
+         }
+    }
 }
